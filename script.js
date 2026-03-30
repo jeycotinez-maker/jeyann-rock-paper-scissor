27a37db6afec6f0c34b9318eacdb03c204d92e27
jey ann <jeycotinez@gmail.com>
2026-03-30 17:54:14 +0800

    please commit this changes

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ const playerScoreEl = document.getElementById("player-score");
 const computerScoreEl = document.getElementById("computer-score");
 const statusTextEl = document.getElementById("status");
 const roundResultEl = document.getElementById("round-result");
+
+const choiceBtns = document.querySelectorAll(".choice-btn");
