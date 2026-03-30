9ae3cfb61a1ee0d0d3cfa1aa0c865d4d9982a90f
jeycotinez-maker <jeycotinez@gmail.com>
2026-03-30 17:43:28 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const playerScoreEl = document.getElementById("player-score");
+const computerScoreEl = document.getElementById("computer-score");
+const statusTextEl = document.getElementById("status");
+const roundResultEl = document.getElementById("round-result");
